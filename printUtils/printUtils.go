e5fd21ac2a4ebe99d77db59eb16e6f6d95622624
adaviloper <16654653+adaviloper@users.noreply.github.com>
2025-10-18 22:48:16 -0500

    updating print to reference config theme

--- printUtils/printUtils.go
+++ printUtils/printUtils.go
@@ -3,30 +3,50 @@
  */
 package printUtils
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
 
-const (
-    Reset  = "\x1b[0m"
-    Red    = "\x1b[31m"
-    Green  = "\x1b[32m"
-    Yellow = "\x1b[33m"
-    Orange = "\x1b[38;5;208m"
-    Bold   = "\x1b[1m"
+	"github.com/fatih/color"
+    "github.com/adaviloper/aoc/config"
 )
 
 func Danger(msg string) {
-    fmt.Println(Bold + Red + msg + Reset)
+	r, g, b, _ := HexToRGB(config.Cfg.Theme.Danger)
+    color.RGB(r, g, b).Println(msg)
 }
 
 func Info(msg string) {
-    fmt.Println(Bold + Yellow + msg + Reset)
+	r, g, b, _ := HexToRGB(config.Cfg.Theme.Info)
+    color.RGB(r, g, b).Println(msg)
 }
 
 func Success(msg string) {
-    fmt.Println(Bold + Green + msg + Reset)
+	r, g, b, _ := HexToRGB(config.Cfg.Theme.Success)
+    color.RGB(r, g, b).Println(msg)
 }
 
 func Warn(msg string) {
-    fmt.Println(Bold + Orange + msg + Reset)
+	r, g, b, _ := HexToRGB(config.Cfg.Theme.Warn)
+    color.RGB(r, g, b).Println(msg)
 }
 
+func HexToRGB(s string) (int, int, int, bool) {                            
+    s = strings.TrimSpace(s)
+    s = strings.TrimPrefix(s, "#")
+    if len(s) == 3 {
+        s = fmt.Sprintf("%c%c%c%c%c%c", s[0], s[0], s[1], s[1], s[2], s[2])
+    }
+    if len(s) != 6 {
+    	return HexToRGB("#cdd6f4")
+    }
+    r, err1 := strconv.ParseUint(s[0:2], 16, 8)
+    g, err2 := strconv.ParseUint(s[2:4], 16, 8)
+    b, err3 := strconv.ParseUint(s[4:6], 16, 8)
+    if err1 != nil || err2 != nil || err3 != nil {
+    	return HexToRGB("#cdd6f4")
+    }
+
+    return int(r), int(g), int(b), true
+}                                                                          
